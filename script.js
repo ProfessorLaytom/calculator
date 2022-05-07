bf00b8c55e9a4a51986790f9d1b812f90256df18
yelowt <tom.dangleterre@gmail.com>
2022-05-07 19:28:05 +0200

    added chained operations and All Clear button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let keyHistory = [0, 0, 0]
+let keyHistory = [0, 0, 0] 
+/// keyHistory: first number - operation - second number, all
+/// strings except when no values are entered
 let isResultDisplayed = false
 
 function add(T){
@@ -6,7 +8,6 @@ function add(T){
         T[0] = `${Math.round((+a + +b)*10)/10}`
         T[1] = 0
         T[2] = 0
-        isResultDisplayed = true
         return +T[0]
 }
 
@@ -15,7 +16,6 @@ function subtract(T){
         T[0] = `${Math.round((+a - +b)*10)/10}`
         T[1] = 0
         T[2] = 0
-        isResultDisplayed = true
         return +T[0]
 }
 
@@ -24,7 +24,6 @@ function multiply(T){
         T[0] = `${Math.round((+a * +b)*10)/10}`
         T[1] = 0
         T[2] = 0
-        isResultDisplayed = true
         return +T[0]
 }
 
@@ -33,11 +32,11 @@ function divide(T){
         T[0] = `${Math.round((+a / +b)*10)/10}`
         T[1] = 0
         T[2] = 0
-        isResultDisplayed = true
         return +T[0]
 }
 
 function operate(T){
+    isResultDisplayed = true
     const toDo = T[1]
     if (toDo == '+'){
         return add(T)
@@ -53,28 +52,47 @@ function operate(T){
 function updateDisplay(e){
     const display = document.querySelector('.display')
     const displayText = display.textContent.trim()
-    const displayValue = +displayText;
+    const displayValue = +displayText; //is the value BEFORE updating it
     const pressed = e.target.textContent
-    display.textContent = pressed
+    // console.log(pressed, displayValue)
+    display.textContent = pressed //is the value AFTER updating the display
     if (isNaN(pressed)){ //if pressed is not a number
         if (pressed === '='){
             display.textContent = operate(keyHistory)
-            isResultDisplayed = false;
             return;
         }
-        else {
+        if (pressed == 'AC'){
+            keyHistory = [0,0,0];
+            isResultDisplayed = false;
+            display.textContent = 0;
+        }
+        [a, b, c] = keyHistory.map(x => typeof(x));
+        if (a == 'string' && b == 'string' && c == 'string'){ //for chaining operation, gives the result and the next operation in small
+            display.textContent = operate(keyHistory);
+            isResultDisplayed = false;
+            keyHistory[1] = pressed;
+            const p = document.createElement('p');
+            p.textContent = pressed
+            p.style.fontSize = '20px'
+            display.appendChild(p)
+            return
+        } else if (pressed != 'AC'){
             keyHistory[1] = pressed
             isResultDisplayed = false;
             return;
         }
-    }  else { //if we chose a number
-        if (displayValue === 0 || isResultDisplayed){
+    }  else { //if we press a number
+        if (displayValue === 0 || isResultDisplayed){ //
             keyHistory[0] = pressed
             isResultDisplayed = false
             return;
         } else if (keyHistory[0] === 0){
-            if (keyHistory[1] === 0){ //updates the first number
+            if (keyHistory[1] === 0){ //updates the first number if no operation
                 keyHistory[0] = pressed
+                return;
+            } else {
+                keyHistory[2] = pressed;
+                keyHistory[0] = `${keyHistory[0]}`
                 return;
             }
         } else if (keyHistory[1] == 0 && !isResultDisplayed){ //gives us multi-decimal numbers for the first number
@@ -90,7 +108,7 @@ function updateDisplay(e){
                 keyHistory[2] += pressed
                 display.textContent = keyHistory[2]
                 return;
-            } else if (isResultDisplayed){
+            } else if (isResultDisplayed){ //removes the last result for the new value entered
                 keyHistory[0] = pressed
                 display.textContent = pressed
                 return;
